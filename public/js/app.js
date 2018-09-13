8b25e122a279483cdf814df618cf1185fa6731fc
taras kumpanenko <t.kumpanenko@gmail.com>
2018-09-13 22:59:55 +0300

    generate content

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+$(function(){
+  var $grid = $('.grid');
+  $grid.isotope({
+      itemSelector: '.element-item'
+  });
+  $('#filters a').click(function(){
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+    $('#filters a').removeClass('active'); 
+    $(this).toggleClass('active');
+    return false;
+  });
+});
