427b52baac2145f878920d4ac0f53d118fdaa8b0
taras kumpanenko <t.kumpanenko@gmail.com>
2018-09-14 22:52:38 +0300

    fixes

--- content/_index.md
+++ content/_index.md
@@ -1,6 +1,7 @@
 +++
 title = ""
 +++
+
 In six short years, Daniel Wellington has built a massive community of influencers and followers whose commitment and loyalty means everything. Now, we want to engage developers all over the world.
 
 At Daniel Wellington, we use various open source projects?anything from what has been time-tested all the way to the bleeding edge.
--- themes/dw/static/js/app.js
+++ themes/dw/static/js/app.js
@@ -1,8 +1,11 @@
 $(function(){
   var $grid = $('.grid');
   $grid.isotope({
-      itemSelector: '.element-item'
+      itemSelector: '.element-item',
+      resizable: false, // disable normal resizing
+      layoutMode: 'fitRows' 
   });
+  
   $('#filters a').click(function(){
     var filterValue = $(this).attr('data-filter');
     $grid.isotope({ filter: filterValue });
