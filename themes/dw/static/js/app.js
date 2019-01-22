c8fc8afa2f5bb7c49dd2d3ff179fcd39738771b4
taras <tk@wwwest.solutions>
2019-01-22 13:36:33 +0200

    ga code added; parse jobs feed from rss using ajax

--- themes/dw/static/js/app.js
+++ themes/dw/static/js/app.js
@@ -1,16 +1,42 @@
-$(function(){
+$(function () {
   var $grid = $('.grid');
   $grid.isotope({
-      itemSelector: '.element-item',
-      resizable: false, // disable normal resizing
-      layoutMode: 'fitRows' 
+    itemSelector: '.element-item',
+    resizable: false, // disable normal resizing
+    layoutMode: 'fitRows'
   });
-  
-  $('#filters a').click(function(){
+
+  $('#filters a').click(function () {
     var filterValue = $(this).attr('data-filter');
     $grid.isotope({ filter: filterValue });
-    $('#filters a').removeClass('active'); 
+    $('#filters a').removeClass('active');
     $(this).toggleClass('active');
     return false;
   });
+
+  //parse job feed
+  var feed = "//careerseurope.danielwellington.com/jobs.rss?department_id=21328";
+
+  $.ajax(feed, {
+    accepts: {
+      xml: "application/rss+xml"
+    },
+    dataType: "xml",
+    success: function (data) {
+      if (!$(data).find("item").length > 0) {
+        var html = '<ul class="carrier-list">';
+        $(data).find("item").each(function () { // or "item" or whatever suits your feed
+          var el = $(this);
+          html += '<li><a href="'+el.find("link").text()+'" target="_blank">'+el.find("title").text()+'</a>';
+          // html += '    <span>{{ .location }}</span>';
+          html += '</li>';
+        });
+        html += '</ul>';
+        $('#career-list').html(html);
+      } else {
+        $('.js--empty-job-list').hide();
+      }
+    }
+  });
+
 });
